dc571c4176b4124c686ff4e04c69d476416f7260
Ryan <ryanhartz9169@gmail.com>
2024-09-07 18:09:26 -0500

    Updated hello.txt with a new line.

--- main.java
+++ main.java
@@ -1,11 +1,20 @@
-public class Main {
-	public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args) 
+	{
 		int[] numbers = {1, 2, 3, 4, 5};
 		System.out.println("The average is: " + average(numbers));
 	}
-	// Non-working average function (needs a fix)
-	public static double average(int[] nums) {
-		// Placeholder: This function is incomplete and currently returns 0
-		return 0;
+
+	public static double average(int[] nums) 
+	{
+		int average = 0;
+
+		for(int i = 0; i < nums.length; i++)
+		{
+			average += nums[i];
+		}
+
+		return average/nums.length;
 	}
 }
